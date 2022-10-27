40ce8bf48db372aae4303782d80e5860760fe55d
himanshu01031997 <hs010397@gmail.com>
2022-10-27 14:59:07 +0530

    inserted data in BST

---
+++ Binary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+   public class Binary<T> where T : IComparable<T>
+    {
+        public T Nodedata { get; set; } 
+        public Binary<T> Lefttree { get; set; }
+        public Binary<T> Righttree { get; set; }
+        public Binary(T Nodedata)
+        {
+            this.Nodedata = Nodedata;
+            this.Lefttree = null;
+            this.Righttree = null;
+        }
+        int leftcount=0, rightcount=0;
+        bool result =false;
+        public void add(T items)
+        {
+            T currentnodevalue = this.Nodedata;
+            if ((currentnodevalue.CompareTo(items)) > 0)
+            {
+                if (this.Lefttree == null)
+                    this.Lefttree=new Binary<T>(items);
+                else 
+                    this.Lefttree.add(items);
+               
+            }
+            else
+            {
+                if(this.Righttree == null)
+                    this.Righttree=new Binary<T>(items);
+                else
+                    this.Righttree.add(items);
+            }
+
+        }
+        public void Display()
+        {
+            if (this.Lefttree != null)
+            {
+                this.leftcount++;
+                this.Lefttree.Display();
+            }
+            Console.WriteLine(this.Nodedata.ToString());
+            if(this.Righttree != null)
+            {
+                this.rightcount++;
+                this.Righttree.Display();
+            }
+        }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BinaryTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Binary<int> binary = new Binary<int>(56);
+            binary.add(30);
+            binary.add(70);
+            binary.Display();
+
+
+
+        }
+    }
+}
